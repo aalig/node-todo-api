a5d7941949e912d43d6807e69e7e713e09b029df
Abdurahman Gugssa (Wipro Ltd.) <v-abdgug@microsoft.com>
2017-03-28 12:02:29 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -23,15 +24,33 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) =>{
-        res.send({todos});
+    Todo.find().then((todos) => {
+        res.send({ todos });
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // Check for valid id using isValid
+    if (!ObjectID.isValid(id)) {       
+        // If not valid send a 404 with empty body
+        return res.status(404).send();
+    }  
+    
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((err) => res.status(400).send());
+
 });
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
